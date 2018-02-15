8f4c30fe50a7d95b20b923d9567ffd62d0221d36
Aaron Kimbrell <awk73@msstate.edu>
2018-02-15 15:36:23 -0600

    Added fuctionalities
    Added code to run the completed functions

--- app/app.py
+++ app/app.py
@@ -1,8 +1,11 @@
-import os
-import sys
-os.system('cls')
+import os # pragma: no cover
+import sys # pragma: no cover
+os.system('cls') # pragma: no cover
+from distance import calc_distance # pragma: no cover
+from bmi import calc_bmi # pragma: no cover 
+from retirement import retirement  # pragma: no cover 
 
-def main():
+def main(): # pragma: no cover
 	print("Welcome to Team 1's Profesional Practice Project!\n"
 		  "1. Body Mass Index\n"
 		  "2. Retirement\n"
@@ -29,44 +32,48 @@ def main():
 			print("Invalid input.")
 
 
-def BMI_input():
+def BMI_input(): # pragma: no cover
+	print()
 	feet = input("Enter your height (feet): ")
 	inches = input("Height (inches): ")
 	weight = input("Enter your weight in pounds: ")
 
-	# output
+	bmi_status, bmi = calc_bmi(feet, inches, weight)
+	print(bmi_status,": ", bmi)
+	print()
 
-def retirement_input():
+def retirement_input(): # pragma: no cover
 	age = input("Enter your age: ")
 	salary = input("Enter your annual salary: ")
-	precent = input("Enter your precent saved (10% would be 10): ")
+	precent = input("Enter your percent saved (10% would be 10): ")
 	goal = input("Enter your retirement saving goal: ")
 
-	retirement(age, salary, precent, goal)
+	print(retirement(age, salary, precent, goal))
 
 	# output
 
-def distance_input():
+def distance_input(): # pragma: no cover
+	print()
 	x1 = input("Input your first x point: ")
 	y1 = input("Input your first y point: ")
 	x2 = input("Input your second x point: ")
 	y2 = input("Input your second y point: ")
 
-	distnace(x1, y1, x2, y2)
+	dist = calc_distance(x1, y1, x2, y2)
+	print("The shortest distance between the two points is ", dist)
+	print()
 
-	# output
-
-def email_input():
+def email_input(): # pragma: no cover
 	email = input("Enter your email address: ")
 
 	verify_email(email)
 
 	# output
 
-def tip_input():
+def tip_input(): # pragma: no cover
 	bill = input("Enter the ammount for the check: ")
 	people = input("Enter the number of people in your party: ")
 
 	# output
 
-main()
+main() # pragma: no cover
