840ee83a03a85ca8533f5412d19c4dccb0d09d4a
Jahrell Harris <jahrell13@gmail.com>
2018-02-15 18:48:59 -0600

    Tip file
    function to calculate tips

---
+++ app/tip.py
@@ -0,0 +1,22 @@
+def split_tip(tip,guest_count):
+        
+		tip_split_var = tip / guest_count
+		guest_array=[round(tip_split_var,2)]
+		total = tip - guest_array[0]
+		for value in range(2,guest_count):
+			guest_array.append(round(tip_split_var,2))
+			total = total - guest_array[value-1]
+		guest_array.append(round(total,2))
+
+
+
+		return guest_array
+
+def main():
+	
+		total = split_tip(15.18, 3)
+
+		for value in range(0,len(total)):
+			print(total[value])
+
+main()
