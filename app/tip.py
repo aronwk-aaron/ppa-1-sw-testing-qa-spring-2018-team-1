1f2016787f6a50e1bf5aa7e724cdd4f5b0c57593
Aaron Kimbrell <awk73@msstate.edu>
2018-02-16 19:40:53 -0600

    Fixed tip
    Removed code that was not tested and assured 100% code coverage.

--- app/tip.py
+++ app/tip.py
@@ -23,19 +23,16 @@ def split_tip(tip, guest_count):
     or ((float(guest_count)) != round(float(guest_count))):
         return False
 
-    if float(tip) == round(float(tip)):
-        return False
-
 	#If given valid input calculates tip values for each guest rounded to 2 decimal places
-    else:
-        guest_count = int(guest_count)
-        tip = float(tip)
-        tip_split_var = tip / guest_count
-        guest_array = [round(tip_split_var, 2)]
-        total = tip - guest_array[0]
-        for value in range(2, guest_count):
-            guest_array.append(round(tip_split_var, 2))
-            total = total - guest_array[value - 1]
-        guest_array.append(round(total, 2))
 
-        return guest_array
+    guest_count = int(guest_count)
+    tip = float(tip)
+    tip_split_var = tip / guest_count
+    guest_array = [round(tip_split_var, 2)]
+    total = tip - guest_array[0]
+    for value in range(2, guest_count):
+        guest_array.append(round(tip_split_var, 2))
+        total = total - guest_array[value - 1]
+    guest_array.append(round(total, 2))
+
+    return guest_array
